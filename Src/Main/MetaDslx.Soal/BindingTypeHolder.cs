29eb33425c2fc857f3f08eb4a7ee64dc13f16b7f
Bubesz <bubesz@gmail.com>
2016-03-08 12:47:40 +0100

    created a binding type holder class

---
+++ Src/Main/MetaDslx.Soal/BindingTypeHolder.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetaDslx.Soal
+{
+    class BindingTypeHolder
+    {
+        private bool restBinding = false;
+        private bool webServiceBinding = false;
+        private bool webSocketBinding = false;
+
+        public bool HasRestBinding
+        {
+            get
+            {
+                return restBinding;
+            }
+
+            set
+            {
+                restBinding = value;
+            }
+        }
+
+        public bool HasWebServiceBinding
+        {
+            get
+            {
+                return webServiceBinding;
+            }
+
+            set
+            {
+                webServiceBinding = value;
+            }
+        }
+
+        public bool HasWebSocketBinding
+        {
+            get
+            {
+                return webSocketBinding;
+            }
+
+            set
+            {
+                webSocketBinding = value;
+            }
+        }
+    }
+}
